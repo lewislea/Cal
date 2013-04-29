d734d7f2b7bd62a4c67ffbbbfce2a393dc0e073a
lewislea <wlewislea@gmail.com>
2013-04-29 10:08:11 -0500

    first go at listing needed tests

--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -6,13 +6,49 @@ class CalTest < Test::Unit::TestCase
   assert_equal `cal 2 2013`, `ruby cal.rb 2 2013`
   end
 
-  def test_01_number_of_days_in_week
+  def test_number_of_days_in_week
   end
 
-  def test_02_number_of_days_in_month
+  def test_number_of_days_in_january
   end
 
-  def test_03_names_of_days
+  def test_number_of_days_in_february
+  end
+
+  def test_number_of_days_in_february_leap_year
+  end
+
+  def test_number_of_days_in_march
+  end
+
+  def test_number_of_days_in_april
+  end
+
+  def test_number_of_days_in_may
+  end
+
+  def test_number_of_days_in_june
+  end
+
+  def test_number_of_days_in_july
+  end
+
+  def test_number_of_days_in_august
+  end
+
+  def test_number_of_days_in_september
+  end
+
+  def test_number_of_days_in_october
+  end
+
+  def test_number_of_days_in_november
+  end
+
+  def test_number_of_days_in_december
+  end
+
+  def test_names_of_days
   end
 
   def test_names_of_month
@@ -21,10 +57,33 @@ class CalTest < Test::Unit::TestCase
   def test_month_displayed_top
   end
 
+  def test_first_letter_month_capitalized
+  end
+
   def test_month_centered
   end
 
-  def
+  def test_numbers_right_justified
+  end
+
+  def test_year_divided_by_100_not_leap_year
+  end
+
+  def test_year_divided_by_400_leap_year
+  end
+
+  def test_year_divided_by_4_leap_year
+  end
+
+  def test_
+  end
+
+  def test_
+  end
+
+  def test_
+  end
+
 
 
 
